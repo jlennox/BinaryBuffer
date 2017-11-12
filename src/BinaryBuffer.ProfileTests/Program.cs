d7d48ff6961d57b81c57755497814f44ce0ec35b
jlennox <jlennox@gmail.com>
2017-11-12 11:11:09 -0800

    Add additional performance profiling tests.

--- src/BinaryBuffer.ProfileTests/Program.cs
+++ src/BinaryBuffer.ProfileTests/Program.cs
@@ -12,20 +12,73 @@ namespace BinaryBuffer.ProfileTests
     {
         private const int _times = 5000000;
 
+        private static int _offset = 2;
+
         private static void Main(string[] args)
         {
-            Console.WriteLine("Read tests:");
-            Time(ReadInt);
-            Time(ReadIntUnsafe);
-            Time(ReadIntUnsafePtr);
-            Time(ReadIntUnsafeVar);
-            Time(BitConverter.ToInt32);
-            Time(ToInt32);
-            Console.WriteLine("Write tests:");
-            Time(WriteIntUnsafe);
-            Time(WriteIntUnsafeShifted);
+            // Run ten times to improve consistency.
+            for (var i = 0; i < 10; ++i)
+            {
+                // Some methods special case aligned access, in theory for
+                // added speed.
+                foreach (var offset in new[] { 2, 0 })
+                {
+                    _offset = offset;
+                    var title = offset != 0
+                        ? "Unaligned access:"
+                        : "Aligned access:";
+
+                    Console.WriteLine(title);
+                    Console.WriteLine(new string('=', title.Length));
+                    Console.WriteLine("Read tests:");
+                    Console.WriteLine("-----------");
+                    Time(ReadInt);
+                    Time(ReadIntUnsafe);
+                    Time(ReadMemory);
+                    Time(ReadIntWithBitCheckUnsafe);
+                    Time(ReadIntWithModCheckUnsafe);
+                    Time(ReadIntUnsafePtr);
+                    Time(ReadIntUnsafeVar);
+                    Time(BitConverter.ToInt32);
+                    Time(ToInt32);
+                    Console.WriteLine("Write tests:");
+                    Console.WriteLine("-----------");
+                    Time(WriteIntUnsafe);
+                    Time(WriteIntUnsafeShifted);
+                    Console.WriteLine("Read network:");
+                    Console.WriteLine("-----------");
+                    Time(ReadNetworkIntUnsafe, false);
+                    Time(ReadNetworkMemory, false);
+                    Console.WriteLine();
+                }
+            }
 
             Console.ReadLine();
+        }
+
+        public static int ReadNetworkIntUnsafe(
+            byte[] buffer, int offset)
+        {
+            fixed (byte* bufferPtr = &buffer[offset])
+            {
+                return ((int)bufferPtr[0] << 24) |
+                    ((int)bufferPtr[1] << 16) |
+                    ((int)bufferPtr[2] << 8) |
+                    ((int)bufferPtr[3] << 0);
+            }
+        }
+
+        public static int ReadNetworkMemory(
+            byte[] buffer, int offset)
+        {
+            fixed (byte* bufferPtr = &buffer[offset])
+            {
+                var result = *(int*)bufferPtr;
+                return ((result/*  & 0x000000FF */) << 24) |
+                    ((result & 0x0000FF00) << 8) |
+                    ((result & 0x00FF0000) >> 8) |
+                    ((int)(result/* & 0xFF000000*/) >> 24);
+            }
         }
 
         public static void WriteIntUnsafe(
@@ -56,10 +109,10 @@ namespace BinaryBuffer.ProfileTests
         public static int ReadInt(
             byte[] buffer, int offset)
         {
-            return ((int)buffer[0] << 0) |
-                ((int)buffer[1] << 8) |
-                ((int)buffer[2] << 16) |
-                ((int)buffer[3] << 24);
+            return ((int)buffer[offset + 0] << 0) |
+                ((int)buffer[offset + 1] << 8) |
+                ((int)buffer[offset + 2] << 16) |
+                ((int)buffer[offset + 3] << 24);
         }
 
         public static int ReadIntUnsafe(
@@ -67,6 +120,49 @@ namespace BinaryBuffer.ProfileTests
         {
             fixed (byte* bufferPtr = &buffer[offset])
             {
+                return ((int)bufferPtr[0] << 0) |
+                    ((int)bufferPtr[1] << 8) |
+                    ((int)bufferPtr[2] << 16) |
+                    ((int)bufferPtr[3] << 24);
+            }
+        }
+
+        public static int ReadMemory(
+            byte[] buffer, int offset)
+        {
+            fixed (byte* bufferPtr = &buffer[offset])
+            {
+                return *(int*)bufferPtr;
+            }
+        }
+
+        public static int ReadIntWithBitCheckUnsafe(
+            byte[] buffer, int offset)
+        {
+            fixed (byte* bufferPtr = &buffer[offset])
+            {
+                if ((offset & 0b11) == 0 && BitConverter.IsLittleEndian)
+                {
+                    return *(int*)bufferPtr;
+                }
+
+                return ((int)bufferPtr[0] << 0) |
+                    ((int)bufferPtr[1] << 8) |
+                    ((int)bufferPtr[2] << 16) |
+                    ((int)bufferPtr[3] << 24);
+            }
+        }
+
+        public static int ReadIntWithModCheckUnsafe(
+            byte[] buffer, int offset)
+        {
+            fixed (byte* bufferPtr = &buffer[offset])
+            {
+                if ((offset % 4) == 0 && BitConverter.IsLittleEndian)
+                {
+                    return *(int*)bufferPtr;
+                }
+
                 return ((int)bufferPtr[0] << 0) |
                     ((int)bufferPtr[1] << 8) |
                     ((int)bufferPtr[2] << 16) |
@@ -120,24 +216,36 @@ namespace BinaryBuffer.ProfileTests
             }
         }
 
-        static void Time(Func<byte[], int, int> a)
+        static void Time(Func<byte[], int, int> a, bool check = true)
         {
             var buff = new byte[] { 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7 };
 
-            for (var i = 0; i < _times / 2; i++)
+            if (check)
             {
-                a(buff, 2);
+                var expected = BitConverter.ToInt32(buff, _offset);
+                var actual = a(buff, _offset);
+
+                if (expected != actual)
+                {
+                    throw new Exception($"Expected: {expected}, got: {actual}. Unable to test {a.Method.Name}");
+                }
             }
 
+            for (var i = 0; i < _times / 2; i++)
+            {
+                a(buff, _offset);
+            }
+
+            GC.TryStartNoGCRegion(0xFFFF);
             var watch = Stopwatch.StartNew();
             for (var i = 0; i < _times; i++)
             {
-                a(buff, 2);
+                a(buff, _offset);
             }
-
             var length = watch.Elapsed;
+            GC.EndNoGCRegion();
 
-            Console.WriteLine("{0}:\t {1}", a.Method.Name, length);
+            Console.WriteLine("{0,-25} {1}", a.Method.Name, length);
         }
 
         static void Time(Action<byte[], int, int> a)
@@ -146,18 +254,19 @@ namespace BinaryBuffer.ProfileTests
 
             for (var i = 0; i < _times / 2; i++)
             {
-                a(buff, 2, 0x22334455);
+                a(buff, _offset, 0x22334455);
             }
 
+            GC.TryStartNoGCRegion(0xFFFF);
             var watch = Stopwatch.StartNew();
             for (var i = 0; i < _times; i++)
             {
-                a(buff, 2, 0x22334455);
+                a(buff, _offset, 0x22334455);
             }
-
             var length = watch.Elapsed;
+            GC.EndNoGCRegion();
 
-            Console.WriteLine("{0}:\t {1}", a.Method.Name, length);
+            Console.WriteLine("{0,-25} {1}", a.Method.Name, length);
         }
     }
 }
